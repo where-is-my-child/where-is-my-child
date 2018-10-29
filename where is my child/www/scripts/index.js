52b06ed1228c45840e1f92c44a31ab303353564e
hassan <h.fm@live.com>
2018-10-29 13:56:34 +0100

    first commit

---
+++ where is my child/www/scripts/index.js
@@ -0,0 +1,49 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        // navigator.vibrate(500)
+        var x = document.getElementById("demo");
+        var btn = document.getElementById('locationBtn');
+
+        btn.addEventListener('click', () => {
+            setInterval(getLocation, 10000);
+        })
+
+        function getLocation() {
+            navigator.geolocation.getCurrentPosition((position) => {
+                console.log(position.coords.longitude);
+            }, () => { alert('no position found...') });
+            if (navigator.geolocation) {
+                navigator.geolocation.watchPosition(showPosition);
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+
+        }
+
+        function showPosition(position) {
+            x.innerHTML = "Latitude: " + position.coords.latitude +
+                "<br>Longitude: " + position.coords.longitude;
+        }
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
